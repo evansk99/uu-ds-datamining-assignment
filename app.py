83a65cf26a0448be3d43c94d8a7f87635291a8f3
Radis Toumpalidis <rtoumpalidis@gmail.com>
2025-10-01 18:11:50 +0000

    finalize preprocessing and vectorization with uni and bi grams

--- app.py
+++ app.py
@@ -6,26 +6,14 @@ import re
 import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.utils import shuffle
+from models import LogRegCV, MultinomialNaiveBayes, vectorize
 
 nltk.download('stopwords') # comment out after first run
 nltk.download('punkt_tab') # comment out after first run
 # Get English stopwords
 stop_words = set(stopwords.words('english'))
-
-
-def LogReg(X: np.array, y: np.array, with_bigrams=False):
-    if with_bigrams:
-        return
-    
-    model = LogisticRegression(random_state=0)
-    classifier = model.fit(X,y)
-    preds = classifier.predict(X)
-    probs = classifier.predict_proba(X)
-    logits = classifier.predict_log_proba(X)
-    raise ValueError
 
 
 def parse_to_pandas(data_dir: str):
@@ -53,10 +41,10 @@ def remove_puncuation(text: str):
     clean_text = "".join([char for char in text if char not in string.punctuation])
     return clean_text
 
+def remove_nums(text: str):
+    return "".join([i for i in text if not i.isdigit()])
+
 def remove_stopwords(text: str):
-    # remove numbers
-    for n in ['0','1','2','3','4','5','6','7','8','9']:
-        text = text.replace(n, '') if n in text else text
     # Remove extent white spaces
     text = re.sub(' +', ' ', text)
     tokens = word_tokenize(text.lower())
@@ -78,32 +66,31 @@ def main():
     df['text'] = df['txt_path'].map(lambda path: open(path,'r').read())
     df['text_processed'] = df['text'].map(lambda text: remove_puncuation(text))
     df['text_processed'] = df['text_processed'].map(lambda text: text.lower())
-    df['text_processed_tokens'] = df['text_processed'].map(lambda text: remove_stopwords(text))
-    # shuffle
-    df = shuffle(df, random_state=42)
-    
-    # 2.5 text vectorization
+    df['text_processed'] = df['text_processed'].map(lambda text: remove_nums(text))
+    df['text_processed'] = df['text_processed'].map(lambda text: remove_stopwords(text))
     train_indexes = df[df['fold'] != 'fold5']['index'].tolist()
     test_indexes = df[df['fold'] == 'fold5']['index'].tolist()
+    
+    # 2.5 text vectorization
     # remove words that appear in less thn 5% of the dataset
-    vect = CountVectorizer(min_df=.05)
-    vects = vect.fit_transform(df.text_processed_tokens)
-    td = pd.DataFrame(vects.todense())
-    td.columns = vect.get_feature_names_out()
-    td['doc'] = df['txt_path'].map(lambda p: p.split('/')[-1])
-    # convert to matrix
-    dtm = td.to_numpy()[:, :-1]
+    dtm_unigrams = vectorize(df)
+    dtm_bigrams = vectorize(df, with_bigrams=True)
     
     # 3. labeling 0: deceptive, 1: truthful
     labels = np.array(df['txt_path'].map(lambda path: np.array(0) if 'deceptive' in path else np.array(1)).tolist())
     
-    # 4. split 
-    X_train, X_test = dtm[train_indexes, :], dtm[test_indexes, :]
+    # 4. split
+    unigrams_ds = {'X_train': dtm_unigrams[train_indexes, :], 'X_test': dtm_unigrams[test_indexes, :]}
+    bigrams_ds = {'X_train': dtm_bigrams[train_indexes, :], 'X_test': dtm_bigrams[test_indexes, :]}
     y_train, y_test = labels[train_indexes], labels[test_indexes]
     
-    # Run Model
-    LogReg(X_train, y_train)
+    # Run Logistic Regression CV
+    LogRegCV(**unigrams_ds,y_train=y_train , y_test=y_test)
+    LogRegCV(**bigrams_ds,y_train=y_train , y_test=y_test, with_bigrams=True)
     
+    # Run Multinomial NB
+    MultinomialNaiveBayes(**unigrams_ds,y_train=y_train , y_test=y_test)
+    MultinomialNaiveBayes(**bigrams_ds,y_train=y_train , y_test=y_test, with_bigrams=True)
     
 if __name__ == '__main__':
     main()
